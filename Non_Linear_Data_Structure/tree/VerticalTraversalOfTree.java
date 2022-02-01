67f47eee29fcf775df57ad1af4524b44409817be
GulabSinghSikarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2022-02-01 19:02:02 +0530

    before zig -zag

--- Non_Linear_Data_Structure/tree/VerticalTraversalOfTree.java
+++ Non_Linear_Data_Structure/tree/VerticalTraversalOfTree.java
@@ -1,30 +1,33 @@
 package tree;
+
 import java.util.*;
 
 public class VerticalTraversalOfTree {
-    
 
-class Node {
+    class Node {
 
-    Node left;
-    Node right;
-    int data;
+        Node left;
+        Node right;
+        int data;
 
-    Node(int data){
-        this.data=data;
+        Node(int data) {
+            this.data = data;
 
+        }
     }
-}
-class Values{
-    int max;
-    int min;
-    Values(int min,int max){
-        this .min=min;
-        this.max=max;
 
+    class Values {
+        int max;
+        int min;
+
+        Values(int min, int max) {
+            this.min = min;
+            this.max = max;
+
+        }
     }
-}
-ArrayList<Integer> list=new ArrayList<>();
+
+    ArrayList<Integer> list = new ArrayList<>();
 
 public static void getTopView(Node root,Values values,int level,ArrayList<Integer> list){
     if(root==null)
@@ -48,38 +51,32 @@ public static void getTopView(Node root,Values values,int level,ArrayList<Intege
 
 }
 
-public ArrayList <Integer> bottomView(Node root)
-{
-    // Code here
-    ArrayList <Integer> list =new ArrayList<>();
+    public ArrayList<Integer> bottomView(Node root) {
+        // Code here
+        ArrayList<Integer> list = new ArrayList<>();
 
-    int level =0;
+        int level = 0;
 
-    TreeMap<Integer,Integer>map=new TreeMap<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
 
-     bottom(root,level,map);
-     for(int key:map.keySet())
-     {
-         list.add(map.get(key));
-     }
-     
+        bottom(root, level, map);
+        for (int key : map.keySet()) {
+            list.add(map.get(key));
+        }
 
-return list ;}
-
-private void bottom(Node root, int level, TreeMap<Integer, Integer> map) {
-    if(root==null)
-    return ;
-    bottom(root.left, level-1, map);
-    bottom(root.right, level+1, map);
-    if(!map.containsKey(level))
-    {
-        map.put(level, root.data);
-        System.out.println(root.data);
+        return list;
     }
-    return;
-    
-    
-}
-}
 
+    private void bottom(Node root, int level, TreeMap<Integer, Integer> map) {
+        if (root == null)
+            return;
+        bottom(root.left, level - 1, map);
+        bottom(root.right, level + 1, map);
+        if (!map.containsKey(level)) {
+            map.put(level, root.data);
+            System.out.println(root.data);
+        }
+        return;
 
+    }
+}
